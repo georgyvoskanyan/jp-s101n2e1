793d112b38e4bc1e3544de02cea7468ea6ebe127
Georgy Voskanyan <hello@voskanyan.dev>
2024-09-16 13:40:38 +0200

    FEAT: Add the base Telephone class

---
+++ src/Telephone.java
@@ -0,0 +1,7 @@
+public class Telephone {
+    private final String brand;
+    private final String model;
+    public void call(String numberBeingCalled) {
+        System.out.printf("S’està trucant al número %s\n", numberBeingCalled);
+    }
+}
